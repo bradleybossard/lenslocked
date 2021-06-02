f9d71d1671daa71788fb9828bb0f4cb23a2e7b13
Bradley Bossard <bradleybossard@gmail.com>
2021-06-01 20:22:24 -0700

    basic web server

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
-func main () {
-  fmt.Println("Hello World!")
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Welcome to my awesome site!</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3001", nil)
 }
